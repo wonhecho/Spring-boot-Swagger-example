4906f06798df7eb4aaaf064f25a90b3358d0031f
wonhecho <chowond02@naver.com>
2021-09-14 02:57:42 +0900

    first commit

---
+++ src/main/java/com/example/swaggerexample/SwaggerexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerexampleApplication.class, args);
+	}
+
+}
